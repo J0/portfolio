8cbe4f5e6f5d90c939069184f6bb41c173be9a5c
joel@joellee.org <joel@joellee.org>
2022-07-24 22:39:17 +0800

    chore: add active links

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,5 +1,23 @@
 import Link from 'next/link';
+import { useRouter } from "next/router";
+
+
 const Navbar = (props: any) => {
+  const router = useRouter();
+
+
+  const links = [
+    {id: 1,
+     url: '/',
+     text: 'home' },
+    {id: 2,
+     url: '/about',
+     text: 'about'},
+    {id: 3,
+     url: '/projects',
+     text: 'projects'}
+  ]
+
 
   return (
     <div className="flex flex-col max-w-screen-xl p-4 mx-auto md:items-center md:justify-between md:flex-row px-2 sm:px-6 lg:px-8">
@@ -10,15 +28,12 @@ const Navbar = (props: any) => {
         </div>
         <div className="hidden sm:block sm:ml-6">
           <div className="flex space-x-4">
-            <Link href="/">
-              <a href="/" className="hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Home</a>
-            </Link>
-            <Link href="/about">
-              <a href="/about" className="hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">About</a>
-            </Link>
-            <Link href="/projects">
-              <a href="/projects" className="hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Projects</a>
-            </Link>
+            {links.map((link) => {
+              const {id, url, text} = link;
+              return (<Link href={url} key={id}>
+                <a href={url} className={`hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium ${router.asPath === url ? 'bg-blue-400' : ''}`}>{text}</a>
+              </Link>)
+            })}
           </div>
         </div>
       </div>
